f48dc36bf77005096eb4a637d625653560f029a5
metehanie <metehan.bolat.ie@gmail.com>
2022-05-10 13:16:20 +0300

    Items

---
+++ app/src/main/java/com/metehanbolat/animatedshuffleverticalgridcompose/Item.kt
@@ -0,0 +1,29 @@
+package com.metehanbolat.animatedshuffleverticalgridcompose
+
+import androidx.compose.ui.graphics.Color
+
+data class Item(
+    val id: Int,
+    val color: Color
+)
+
+private val colors = listOf(
+    Color(0xFFF44336),
+    Color(0xFFE91E63),
+    Color(0xFF9C27B0),
+    Color(0xFF673AB7),
+    Color(0xFF3F51B5),
+    Color(0xFF2196F3),
+    Color(0xFF03A9F4),
+    Color(0xFF00BCD4),
+    Color(0xFF009688),
+    Color(0xFF4CAF50),
+    Color(0xFF8BC34A),
+    Color(0xFFCDDC39),
+    Color(0xFFFFEB3B),
+    Color(0xFFFFC107),
+    Color(0xFFFF9800),
+    Color(0xFFFF5722),
+)
+
+fun createItem(count: Int): List<Item> = (1..count).map { Item(it, colors[it % colors.size]) }
